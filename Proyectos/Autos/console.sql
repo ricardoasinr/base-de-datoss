22c1773701d22399521328f1cb7c70a498ec3b32
Ricardo Asin <ricardoasinr@gmail.com>
2022-05-07 01:18:24 -0400

    Funciones terminadas

--- Proyectos/Autos/console.sql
+++ Proyectos/Autos/console.sql
@@ -6,7 +6,6 @@ CREATE TABLE concesionaria (
   CONSTRAINT PK_conce PRIMARY KEY (NITCS)
 );
 
-
 CREATE TABLE modelo (
   codmodelo varchar(20) NOT NULL,
   marca varchar(20) NOT NULL,
@@ -89,37 +88,42 @@ CREATE TABLE venta (
   CONSTRAINT FK_vendedor FOREIGN KEY (codvendedor) REFERENCES vendedor (codvendedor)
 );
 
-insert into concesionaria values ('45456','Ricardo Automotors','Villa Fatima 23', '7345789');
-insert into concesionaria values ('45633','Ricardo Automotors','Villa Guadalupe 27', '7344789');
-insert into concesionaria values ('34566','Ricardo Automotors','Villa Mercedes 22', '7564322');
 
-insert into serviciosof values ('2222','45456','Monica','Villa Alameda 23', '74522536');
-insert into serviciosof values ('2223','45633','Ricardo','Villa Alameda 22', '74522566');
-insert into serviciosof values ('2224','34566','Fernando','Villa Alameda 53', '74522567');
-
-insert into modelo values ('12345','Mazda','5','36000','5','Japon', '45456');
-insert into modelo values ('12344','Nissan','5','18000','5','Japon', '23432');
-insert into modelo values ('12346','Toyota','5','36000','5','Japon', '67544');
-
-insert into equipo values ('678','Android auto','300');
-insert into equipo values ('787','Android auto','300');
-insert into equipo values ('797','Android auto','300');
-
-insert into tipoequipo values ('12345','678','Farol');
-
-insert into vendedor values ('555','45456','2222','Jose Mario Santa Cruz','76363458','marito2010@hotmail.com');
-insert into vendedor values ('554','45633','2223','Marcelo Davila','76564333','marcelito2010@hotmail.com');
-insert into vendedor values ('556','34566','2224','Franklin Romero','76854677','franklincito2010@hotmail.com');
+ insert into concesionaria values ('45456','Ricardo Automotors','Villa Fatima 23', '7345789');
+ insert into concesionaria values ('45633','Ricardo Automotors','Villa Guadalupe 27', '7344789');
+ insert into concesionaria values ('34566','Ricardo Automotors','Villa Mercedes 22', '7564322');
+ insert into serviciosof values ('2222','45456','Monica','Villa Alameda 23', '74522536');
+ insert into serviciosof values ('2223','45633','Ricardo','Villa Alameda 22', '74522566');
+ insert into serviciosof values ('2224','34566','Fernando','Villa Alameda 53', '74522567');
+ insert into modelo values ('12345','Mazda','5','36000','5','Japon', '45456');
+ insert into modelo values ('12344','Nissan','5','18000','5','Japon', '23432');
+ insert into modelo values ('12346','Toyota','5','36000','5','Japon', '67544');
+ insert into equipo values ('678','Android auto','300');
+ insert into equipo values ('787','Android auto','300');
+ insert into equipo values ('797','Android auto','300');
 
 
-insert into automovil values ('1111','45456','12345','blanco','20190318','Japon','20');
-insert into automovil values ('12342','45633','12344','rojo','20190418','Japon','15');
-insert into automovil values ('9878','34566','12346','negro','20190518','Japon','5');
+ insert into tipoequipo values ('12345','678','Farol');
 
-insert into serie values ('678','1111','5411');
+ insert into vendedor values ('555','45456','2222','Jose Mario Santa Cruz','76363458','marito2010@hotmail.com');
+ insert into vendedor values ('554','45633','2223','Marcelo Davila','76564333','marcelito2010@hotmail.com');
+ insert into vendedor values ('556','34566','2224','Franklin Romero','76854677','franklincito2010@hotmail.com');
 
-insert into venta values ('3333','555','20190507','Bolivia','20000','Trans');
+ insert into automovil values ('1111','45456','12345','blanco','20190318','Japon','20');
+ insert into automovil values ('12342','45633','12344','rojo','20190418','Japon','15');
+ insert into automovil values ('9878','34566','12346','negro','20190518','Japon','5');
 
+ insert into serie values ('678','1111','5411');
+ insert into venta values ('3333','555','20190507','Bolivia','20000','Trans');
+ insert into venta values ('3334','123','20190507','Bolivia','2500','Trans');
+
+alter table automovil add Estado varchar(10)
+
+update automovil set Estado = 'En Stock' where NroChasis = '1111'
+update automovil set Estado = 'En Stock' where NroChasis = '9878'
+update automovil set Estado = 'Vendido' where NroChasis = '12342'
+
+select * from vendedor;
 select * from concesionaria;
 select * from serviciosof;
 select * from modelo;
@@ -129,3 +133,33 @@ select * from vendedor;
 select * from automovil;
 select * from serie;
 select * from venta;
+
+select Estado from automovil
+
+
+alter table venta add CONSTRAINT FK_Vendedores_Concesionarios FOREIGN KEY(CodVendedor) REFERENCES vendedor
+
+create function ListaVendedores()
+RETURNS SETOF vendedor as $$
+
+        select * from vendedor;
+
+
+$$
+    language sql;
+
+drop procedure ListaVendedores();
+select * from ListaVendedores();
+
+create function verVendedor ()
+RETURNS SETOF vendedor as $$
+
+        select * from vendedor;
+
+
+$$
+    language sql;
+
+
+
+/**/
--- Python projects/Autos/main.py
+++ Python projects/Autos/main.py
@@ -6,7 +6,6 @@ def comp(t):
     print(llamandoTabla)
     return str(llamandoTabla)
 
-
 def llamarTabla(tabla):
     nombreTabla=comp(tabla)
     cursor = connection.cursor()
@@ -18,24 +17,118 @@ def llamarTabla(tabla):
     for row in rows:
         print(row)
 
-   
+def crearVendedor():
+    print("------------------")
+    print("Creando nuevos vendedores: ")
 
+    codigoV = input("Inserte el codigo del vendedor: ")
+    nitcsV = input("Inserte el NIT de la concesionaria: ")
+    nitofV = input("Inserte NIT del servicio oficial: ")
+    nombreV = input("Inserte el nombre del vendedor: ")
+    telfV = input("Inserte el numero del vendedor: ")
+    emailV = input("Inserte el email del vendedor: ")
 
+    print("------------------")
+    try:
+        a = f"insert into vendedor values('{codigoV}', '{nitcsV}', '{nitofV}', '{nombreV}', '{telfV}', '{emailV}')"
+        print(a)
+        cursor.execute(a)
+        connection.commit()
+        print("\n")
+        print("------------------")
+        print("Vendedor creado correctamente")
+        print("------------------")
+        print("\n")
+    except:
+        print("No se pudo crear el vendedor")
 
+def crearTabla():
+    nombreTabla = input("Nombre de la tabla: ")
+    n = nombreTabla
+    #s = str(n)
+    try:
 
+        cursor = connection.cursor()
+        c = f"create table {n} (nombre varchar(30), correo varchar(30), email varchar(30))"
+        cursor.execute(c)
+        cursor.close()
+        connection.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+
+def eliminarTabla(nombreTabla):
+    n = nombreTabla
+    try:
+
+        cursor = connection.cursor()
+        c = f"drop table {n}"
+        cursor.execute(c)
+        cursor.close()
+        connection.commit()
+        print("Eliminada correctamente")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+
+#Creando la conexion
 try:
     connection=psycopg2.connect(
         host='localhost',
-        port=54320,
-        dbname='my_database',
+        port=54321,
+        dbname='postgres',
         user='postgres',
         password='my_password',
     )
     print("Conexion segura")
-    llamarTabla("vendedor")
+    print("------------------")
+
+
+#Llamando a la tabla vendedor
+    #llamarTabla("vendedor")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM vendedor;")
+    row = 0
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print("Codigo: " + row[0], " | Nombre: "+ row[3], " | Telefono: "+ row[4])
+    print("------------------")
     print("\n")
-    llamarTabla("concesionaria")
+
+
+    cursor.callproc('ListaVendedores')
+
+    row = cursor.fetchone()
+
+    while row is not None:
+        print(row)
+        row = cursor.fetchone()
+
+
+
+#Creando un nuevo vendedor:
+    #crearVendedor()
+    #--------------------------
+#Creando tabla
+    #crearTabla()
+    #--------------------------
+#Eliminando tabla
+    #nombreTabla = input("Nombre de la tabla: ")
+    #eliminarTabla(nombreTabla)
+
+
 
     
 except Exception as e:
     print(e)
+
+finally:
+    connection.close()
+    print("\n")
+    print("------------------")
+    print("Conexion finalizada")

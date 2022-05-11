f14ceae55802234f4094d10c1f392a0025a5705a
Ricardo Asin <ricardoasinr@gmail.com>
2022-05-11 02:22:08 -0400

    Registro de vehiculos

--- Python projects/Autos/main.py
+++ Python projects/Autos/main.py
@@ -42,6 +42,75 @@ def crearVendedor():
     except:
         print("No se pudo crear el vendedor")
 
+
+def crearEquipo():
+    print("------------------")
+    print("Registrar equipamiento: ")
+    codequipo = input("Registre el codigo del equipo: ")
+    descripcion = input("Descripcion: ")
+    precio = input("Precio: ")
+    print("------------------")
+    try:
+        a = f"insert into equipo values('{codequipo}', '{descripcion}', '{precio}')"
+        print(a)
+        cursor.execute(a)
+        connection.commit()
+        print("\n")
+        print("------------------")
+        print("Equipo registrado")
+        print("------------------")
+        print("\n")
+    except:
+        print("No se pudo registrar")
+
+def crearTipoEquipo():
+    print("------------------")
+    print("Registre el tipo de equipo: ")
+
+    Codmodelo = input("*Modelo: ")
+    Codequipo = input("*Registre el codigo del equipo: ")
+    Tipoequipo = input("Descripcion: ")
+    print("------------------")
+    try:
+        a = f"insert into tipoequipo values('{Codmodelo}', '{Codequipo}', '{Tipoequipo}')"
+        print(a)
+        cursor.execute(a)
+        connection.commit()
+        print("\n")
+        print("------------------")
+        print("Registrado")
+        print("------------------")
+        print("\n")
+    except:
+        print("No se pudo registrar")
+
+def crearVehiculo():
+    print("------------------")
+    print("Registrando un nuevo vehiculo: ")
+    NroChasis = input("Numero de chasis: ")
+    NITCS = input("*NIT de la concesionaria: ")
+    CodModelo = input("Codigo modelo: ")
+    Color = input("Color: ")
+    print("Por favor introcir de la siguiente manera '20190518' = 18/5/2019 ")
+    FechaFabricacion = input("Fecha: ")
+    LugarFabricacion = input("Lugar de fabricacion: ")
+    Descuento = input("Descuento: ")
+    estado = "En Stock"
+
+    print("------------------")
+    try:
+        a = f"insert into automovil values('{NroChasis}', '{NITCS}', '{CodModelo}', '{Color}', '{FechaFabricacion}', '{LugarFabricacion}', '{Descuento}', '{estado}')"
+        print(a)
+        cursor.execute(a)
+        connection.commit()
+        print("\n")
+        print("------------------")
+        print("Auto registrado correctamente")
+        print("------------------")
+        print("\n")
+    except:
+        print("No se pudo registrar el auto")
+
 def crearTabla():
     nombreTabla = input("Nombre de la tabla: ")
     n = nombreTabla
@@ -75,25 +144,54 @@ def eliminarTabla(nombreTabla):
         if connection is not None:
             connection.close()
 
-def verficarEstado(chasis):
+def verificarEstado(chasis):
     cursor = connection.cursor()
     cursor.execute("SELECT nrochasis, Estado FROM automovil")
     disponibilidad = 0
+    c = chasis
     #cursor.execute("select nrochasis from automovil ")
     row = 0
     rows = cursor.fetchall()
     for row in rows:
-        if ((row[0] == int(chasis)) and (row[1] == ('En Stock'))):
+        if ((row[0] == int(c)) and (row[1] == ('En Stock'))):
             print("Chasis: " + str(row[0]) + " | Estado: " + row[1])
             print("El auto se encuentra disponible")
             disponibilidad = 1
-            
+       
+    if(disponibilidad == 0):
+        print("El auto NO se encuentra disponible")
+        disponibilidad = 0
+
     print("------------------")
-    return(disponibilidad)
+
     print("\n")
+    return (disponibilidad)
 
 def realizarVenta():
-    chasis = input('Ingrese el numero de chasis para verificar disponibilidad: ')
+    chasiss = input('Ingrese el numero de chasis para verificar disponibilidad: ')
+    a = verificarEstado(chasiss)
+    
+    if(a==1):
+        print("La venta puede seguir adelante")
+    else:
+        crearVehiculo()
+        respEq = input("Desea registra un equipo extra? Si/No")
+        if(respEq == "Si"):
+            crearEquipo()
+
+
+        respTeq = input("Desea registra un tipo equipo extra? Si/No ")
+        if (respTeq == "Si"):
+           crearTipoEquipo()
+
+
+
+        respVenta = input("Desea realizar la venta? Si/No ")
+        if(respVenta == 'Si'):
+            print("Iniciando proceso de venta: ")
+        else:
+            print("Gracias por el registro, vuelva pronto ")
+
 
 
 
@@ -127,17 +225,7 @@ try:
     row=0
 #Verificando automoviles
 # Verficando estado del vehiculo para realizar la venta
-    verficarEstado()
-
-
-    #cursor.callproc('ListaVendedores')
-
-    #row = cursor.fetchone()
-
-    #while row is not None:
-     #   print(row)
-      #  row = cursor.fetchone()
-
+    realizarVenta()
 
 
 #Creando un nuevo vendedor:
